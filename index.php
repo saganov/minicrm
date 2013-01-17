2bcb9f49b61a06a3eeaa076f174cd70fd67e6452
Petr Saganov <saganoff@gmail.com>
2013-01-18 01:16:26 +0200

    Adds the code to work with PaaS AppFog

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ F3::set('UI',__DIR__.'/app/views/');
 
 F3::config(__DIR__.'/config/config.ini');
 
+if($services_json = json_decode(getenv("VCAP_SERVICES"),true))
+{
+    $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+    F3::set('DB.user', $mysql_config["username"]);
+    F3::set('DB.password', $mysql_config["password"]);
+    F3::set('DB.host', $mysql_config["hostname"]);
+    F3::set('DB.port', $mysql_config["port"]);
+    F3::set('DB.name', $mysql_config["name"]);
+}
+
 F3::set('DB', new DB('mysql:host='. F3::get('DB.host') .';port='. F3::get('DB.port') .';dbname='. F3::get('DB.name'),
                      F3::get('DB.user'),
                      F3::get('DB.password')));
