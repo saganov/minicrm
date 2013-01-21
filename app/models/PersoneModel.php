b7a2137638ffaa854b2f473961d81933f61cde70
Petr Saganov <saganoff@gmail.com>
2013-01-21 23:45:25 +0200

    Fix bug with empty optional fields in Axon.save method

--- app/models/PersoneModel.php
+++ app/models/PersoneModel.php
@@ -83,11 +83,6 @@ class PersoneModel
     {
         $this->data->copyFrom('POST');
 
-        // `portrait`,`photo1`,`photo2`,`photo3`,`photo4`,`photo5`,`passport`,`show`,`click`
-        foreach(array('portrait','photo1','photo2','photo3','photo4','photo5','passport') as $label)
-        {
-            if(!isset($this->data->$label)) $this->data->$label = 'NULL';
-        }
         $this->data->show  = 0;
         $this->data->click = 0;
 
@@ -100,13 +95,9 @@ class PersoneModel
         $this->data->load('id='.$id);
         $this->data->copyFrom('POST');
 
-        // `portrait`,`photo1`,`photo2`,`photo3`,`photo4`,`photo5`,`passport`,`show`,`click`
-        foreach(array('portrait','photo1','photo2','photo3','photo4','photo5','passport') as $label)
-        {
-            if(!isset($this->data->$label)) $this->data->$label = 'NULL';
-        }
         $this->data->show  = 0;
         $this->data->click = 0;
+
         $this->data->save($id);
         return $id;
     }
