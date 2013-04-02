2f05bfaecc68c7f5ef211a08c4135a64edeb902f
Petr Saganov <saganoff@gmail.com>
2013-04-03 01:27:03 +0300

    refactor all profile views to remove all CSS and JS into separate files

---
+++ ui/js/profile.js
@@ -0,0 +1,152 @@
+(function() {
+    var httpRequest;
+
+    document.forms['profile_add'].onsubmit = function(e){
+        var empty_mandatory = [];
+        for(var max = this.elements.length, i = 0; i < max; i++)
+        {
+            if(hasClass(this.elements[i], 'mandatory') && this.elements[i].value == '')
+            {
+                empty_mandatory.push(this.elements[i].value);
+                this.elements[i].className = this.elements[i].className + " warning";
+            }
+            else
+            {
+                removeClass(this.elements[i], 'warning');
+            }
+        } 
+        
+        var messager = document.getElementById('form_message');
+        messager.innerHTML = "";
+        messages = [];
+        
+        if(empty_mandatory.length)
+        {
+            messages.push("Please fill the mandatory fields");
+        }
+        
+        if(messages.length)
+        {
+            messager.appendChild(document.createTextNode(messages.join("<br/>\n")));
+            return false;
+        }
+        
+        //e.preventDefault();
+        makeRequest('POST', this.getAttribute('action'), new FormData(this));
+        return false;
+        
+        //return true;
+    }
+
+    function makeRequest(method, url, data) {
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
+        }
+        
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open(method, url, true);
+        httpRequest.send(data);
+    }
+    
+    function alertContents() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var response = JSON.parse(httpRequest.responseText);
+                if(response.res !== false)
+                {
+                    switch (response.operation){
+                    case 'insert':
+                        var message = ['New profile was succesfully inserted'];
+                        break; 
+                    case 'update':
+                        var message = ['Profile was succesfully updated'];
+                        window.location.href = '/profiles/list/';
+                        break;
+                    default:
+                        var message = ['Unknown operation: '+ response.operation];
+                    }
+
+                    var color   = 'green';
+                    document.forms['profile_add'].reset()
+                }
+                else
+                {
+                    var message = ['Error occurs during profile saving'];
+                    var color   = 'red';
+                    if(response.invalid && response.invalid.length > 0)
+                    {
+                        var err = [];
+                        for(var max = response.invalid.length, i = 0; i < max; i++)
+                        {
+                            addClass('warning', document.getElementById(response.invalid[i]))
+                            err.push(response.invalid[i]);
+                        }
+                        
+                        message.push('Invalid field(s): ' + err.join(', '));
+                    }
+
+                    if(response.absent && response.absent.length > 0)
+                    {
+                        var err = [];
+                        for(var max = response.absent.length, i = 0; i < max; i++)
+                        {
+                            addClass('warning', document.getElementById(response.absent[i]))
+                            err.push(response.absent[i]);
+                        }
+                        
+                        message.push('Missed mandatory field(s): ' + err.join(', '));
+                    }
+                }
+                
+                var messageElm = document.getElementById('profile_massage');
+                messageElm.setAttribute('style', 'text-align:center;margin:10px;color:'+color);
+                messageElm.innerHTML = '';
+                for(var max=message.length, i=0; i<max; i++)
+                {
+                    //var input_field = document.getElementById(i);
+                    //addClass(input_field, 'warning');
+                    messageElm.appendChild(document.createTextNode(message[i]));
+                    messageElm.appendChild(document.createElement("br"));
+                }
+                console.log(response);
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+    
+    function hasClass(element, cls) {
+        return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+    }
+    
+    function addClass(element, cls){
+        if(!hasClass(element, cls)){
+            element.className = element.className +' '+ cls;
+        }
+    }
+    
+    function removeClass(element, cls){
+        var classes = element.className.split(/\s+/);
+        var idx = classes.indexOf(cls);
+        if(idx > -1)
+        {
+            classes.splice(idx, 1);
+        }
+        
+        element.className = classes.join(" ");
+    }
+})();
---
+++ ui/js/profile_list.js
@@ -0,0 +1,346 @@
+(function() {
+    var httpRequest;
+    makeRequest('GET', '/profiles/manage');
+
+    function setEvent(elm, event, listener){
+        if (elm.addEventListener)
+            elm.addEventListener(event, listener, false); //everything else    
+        else if (elm.attachEvent)
+            elm.attachEvent(event, listener);  //IE only
+
+        return elm;
+    }
+
+    function showUploadVideoPopup(item){
+        var popup   = createElement('DIV', {'class': 'popup'});
+
+        var header  = append(createElement('H3'),
+                             createTextNode('Video for '+item.first_name+' - #'+item.id),
+                             append(setEvent(createElement('A', {'class':'close',
+                                                                 'href': '#close',
+                                                                 'title':'Close'}),
+                                             'click',
+                                            (function(elm){
+                                                return function(e){
+                                                    elm.setAttribute('style', 'opacity:0');
+                                                    setTimeout(function(){document.getElementsByTagName('BODY')[0].removeChild(elm);}, 500);
+                                                    }})(popup)),
+                                    createTextNode('x')));
+        var profile = append(createElement('DIV', {'class': 'avatar'}),
+                             createElement('IMG',
+                                           {'src':    item.portrait,
+                                            'width':  100,
+                                            'height': 133}));
+
+        var fieldset  = append(createElement('FIELDSET'),
+                              append(createElement('OL'),
+                                     append(createElement('LI'),
+                                            append(createElement('LABEL',{'for': 'video'}),
+                                                   createTextNode('Upload New Video:')),
+                                            createElement('INPUT', {'type': 'file',
+                                                                    'name': 'video'})),
+                                     append(createElement('LI'),
+                                            append(createElement('LABEL',{'for': 'title'}),
+                                                   createTextNode('Title:')),
+                                            createElement('INPUT', {'type': 'text',
+                                                                    'name': 'title'}))));
+        var submit  = append(createElement('FIELDSET', {'class': 'submit'}),
+                             append(createElement('OL'),
+                                    append(createElement('LI'),
+                                           createElement('INPUT', {'type': 'submit',
+                                                                   'name': 'submit',
+                                                                   'value': 'Upload'}))));
+        append(document.getElementsByTagName('BODY')[0],
+               append(popup, append(createElement('DIV'), header,  profile, fieldset, submit)));
+
+        setTimeout(function(){popup.setAttribute('style', 'opacity:100');}, 500);
+    }
+
+    function showActivateDeactivate(item){
+        var popup   = createElement('DIV', {'class': 'popup'});
+        
+        var header  = append(
+            append(createElement('H3'),
+                   createTextNode(('active' == item.status ? 'Де-активация' : 'Активация')),
+                   append(setEvent(createElement('A', {'class':'close', 'href': '#close', 'title':'Close'}),
+                                   'click',
+                                   (function(elm){
+                                       return function(e){
+                                           elm.setAttribute('style', 'opacity:0');
+                                           setTimeout(function(){document.getElementsByTagName('BODY')[0].removeChild(elm);}, 500);
+                                       }})(popup)),
+                          createTextNode('x'))));
+        
+        var tostatus = ('active' == item.status ? 'деактивации' :  'активации');
+        var fieldset  = append(createElement('FIELDSET', {'style': 'width:100%;margin:20px 0;padding:0'}),
+                              append(createElement('OL'),
+                                     append(createElement('LI', {'class': 'wide'}),
+                                            append(createElement('LABEL',{'for': 'reason', 'style': 'width:100%;font-weight:bold'}),
+                                                   createTextNode('Причина '+ tostatus +' анкеты '
+                                                                  +'#'+ item.id +' ('+ item.first_name +' '+ item.last_name +'):')),
+                                            createElement('INPUT', {'type': 'text',
+                                                                    'name': 'reason',
+                                                                   'style': 'width:100%'}))));
+        var submit  = append(createElement('FIELDSET', {'class': 'submit'}),
+                             append(createElement('OL'),
+                                    append(createElement('LI'),
+                                           createElement('INPUT', {'type': 'submit',
+                                                                   'name': 'submit',
+                                                                   'value':('active' == item.status ? 'Deactivate' :  'Activate')}))));
+        append(document.getElementsByTagName('BODY')[0],
+               append(popup, append(createElement('DIV'), header,  fieldset, submit)));
+
+        setTimeout(function(){popup.setAttribute('style', 'opacity:100');}, 500);
+    }
+
+    function showFirstletterPopup(item){
+        var popup   = createElement('DIV', {'class': 'popup'});
+        
+        var header  = append(
+            append(createElement('H3'),
+                   createTextNode('Первое письмо'),
+                   append(setEvent(createElement('A', {'class':'close', 'href': '#close', 'title':'Close'}),
+                                   'click',
+                                   (function(elm){
+                                       return function(e){
+                                           elm.setAttribute('style', 'opacity:0');
+                                           setTimeout(function(){document.getElementsByTagName('BODY')[0].removeChild(elm);}, 500);
+                                       }})(popup)),
+                          createTextNode('x'))));
+        
+        var fieldset  = append(createElement('FIELDSET', {'style': 'width:100%;margin:20px 0;padding:0'}),
+                              append(createElement('OL'),
+                                     append(createElement('LI', {'class': 'wide'}),
+                                            append(createElement('LABEL',{'for': 'reason', 'style': 'width:100%;font-weight:bold'}),
+                                                   createTextNode('')),
+                                            createElement('TEXTAREA', {'name': 'first_letter',
+                                                                       'style': 'width:100%;height:90px'}))));
+        var submit  = append(createElement('FIELDSET', {'class': 'submit'}),
+                             append(createElement('OL'),
+                                    append(createElement('LI'),
+                                           createElement('INPUT', {'type': 'submit',
+                                                                   'name': 'submit',
+                                                                   'value':'Save'}))));
+        append(document.getElementsByTagName('BODY')[0],
+               append(popup, append(createElement('DIV'), header,  fieldset, submit)));
+
+        setTimeout(function(){popup.setAttribute('style', 'opacity:100');}, 500);
+    }
+
+
+    function createElement(tag, attributes){
+        attributes = attributes || false;
+        var elm = document.createElement(tag);
+        if(attributes){
+            for(var i in attributes){
+                elm.setAttribute(i, attributes[i]);
+            }
+        }
+        
+        return elm;
+    }
+
+    function createTextNode(text){
+        return document.createTextNode(text);
+    }
+
+
+    function append(parent)
+    {
+        for(var i=1, max=arguments.length; i<max; i++){
+            parent.appendChild(arguments[i]);
+        }
+        return parent;
+    }
+
+    function makeRequest(method, url) {
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
+        }
+        
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open(method, url, true);
+        httpRequest.send();
+    }
+    
+    function alertContents() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var response = JSON.parse(httpRequest.responseText);
+                var container = document.getElementById('profile_list');
+                for(var max=response.length, i=0; i<max; i++){
+                    /*
+                      ID:		
+                      First Name:
+                      Last Name:
+                      Profile: 		View,Edit
+                      Status:		Active, Invisible
+                      Login:		View
+                      Video:		Upload
+                      Firstletter:	Upload
+                      Profile views:	
+                    */
+                    var profile = document.createElement('DIV');
+                    
+                    var item = {id: 'ID', first_name: 'First Name', last_name: 'Last Name'};
+                    var desc = document.createElement('OL');
+                    for(var lbl in item){
+                        var elm = document.createElement('LI');
+                        var label = document.createElement('SPAN');
+                        label.appendChild(document.createTextNode(item[lbl] +': '));
+                        elm.appendChild(label);
+                        var text = document.createElement('SPAN');
+                        text.appendChild(document.createTextNode(response[i][lbl] || lbl));
+                        elm.appendChild(text);
+                        desc.appendChild(elm);
+                    }
+                    
+                    // Profile: 	View,Edit
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Profile: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    var view = document.createElement('A');
+                    view.setAttribute('href', '/profiles/get/'+response[i].id);
+                    view.appendChild(document.createTextNode('view'));
+                    var edit = document.createElement('A');
+                    edit.setAttribute('href', '/profiles/get/'+response[i].id);
+                    edit.appendChild(document.createTextNode('edit'));
+                    text.appendChild(view);
+                    text.appendChild(document.createTextNode('|'));
+                    text.appendChild(edit);
+                    elm.appendChild(text);
+                    desc.appendChild(elm);
+
+                    // Status:		Active, Invisible
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Status: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    var status_text = ('active' == response[i].status ? 'active' : 'inactive');
+                    var status = append(createElement('A', {'href': '#', 'data-status': status_text}),
+                                        createTextNode(status_text));
+                    setEvent(status, 'mouseenter', (function(status){
+                        return function(e){
+                            e.preventDefault();
+                            e.stopPropagation();
+                            e.target.innerHTML = ('active' == status ? 'deactivate?' : 'activate?');
+                        }
+                    })(status_text));
+                    setEvent(status, 'mouseout', (function(status){
+                        return function(e){
+                            e.preventDefault();
+                            e.stopPropagation();
+                            e.target.innerHTML = this.getAttribute('data-status');
+                        }
+                    })(status_text));
+                    setEvent(status, 'click', (function(item){
+                        return function(e){
+                            e.preventDefault();
+                            e.stopPropagation();
+                            showActivateDeactivate(item);
+                        }
+                    })(response[i]));
+                    elm.appendChild(append(text, status));
+                    desc.appendChild(elm);
+
+                    // Login:		View
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Login: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    var view = document.createElement('A');
+                    view.setAttribute('href', '#');
+                    setEvent(view, 'click', (function(item){return function(e){
+                        //e.stopImmediatePropagation();
+                        e.preventDefault();
+                        e.stopPropagation();
+                        alert("Login: "+ item.email +"\nPassword: "+ item.password);}})(response[i]));
+                    view.appendChild(document.createTextNode('view'));
+                    text.appendChild(view);
+                    elm.appendChild(text);
+                    desc.appendChild(elm);                    
+                    
+                    //  Video:		Upload
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Video: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    var upload = document.createElement('A');
+                    upload.setAttribute('href', '#');
+                    setEvent(upload, 'click', (function(item){return function(e){
+                        //e.stopImmediatePropagation();
+                        e.preventDefault();
+                        e.stopPropagation();
+                        showUploadVideoPopup(item)}})(response[i]));
+                    upload.appendChild(document.createTextNode('upload'));
+                    text.appendChild(upload);
+                    elm.appendChild(text);
+                    desc.appendChild(elm);                    
+
+                    //  Firstletter:	Upload
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Firstletter: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    var upload = document.createElement('A');
+                    upload.setAttribute('href', '#');
+                    setEvent(upload, 'click', (function(item){return function(e){
+                        //e.stopImmediatePropagation();
+                        e.preventDefault();
+                        e.stopPropagation();
+                        showFirstletterPopup(item)}})(response[i]));
+                    upload.appendChild(document.createTextNode('upload'));
+                    text.appendChild(upload);
+                    elm.appendChild(text);
+                    desc.appendChild(elm);
+
+                    //  Profile views:
+                    var elm = document.createElement('LI');
+                    var label = document.createElement('SPAN');
+                    label.appendChild(document.createTextNode('Profile views: '));
+                    elm.appendChild(label);
+                    var text = document.createElement('SPAN');
+                    text.appendChild(document.createTextNode(response[i]['show']));
+                    elm.appendChild(text);
+                    desc.appendChild(elm);
+
+
+                    var link = document.createElement('A');
+                    link.setAttribute('href', '/profiles/get/'+response[i].id);
+                    var avatar = document.createElement('IMG');
+                    avatar.setAttribute('src', response[i].portrait);
+                    avatar.setAttribute('width', 100);
+                    avatar.setAttribute('height', 133);
+                    link.appendChild(avatar);
+
+                    profile.appendChild(link);
+                    profile.appendChild(desc);
+                    container.appendChild(profile);
+                }
+                console.log(response);
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+})();
